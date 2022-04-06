50022e5d2e32c7e675564dfc8d46614a8ba2a571
jp0301 <jp0301@naver.com>
2022-04-06 15:11:18 +0900

    Category Select Func Update 22-04-06

--- main.js
+++ main.js
@@ -15,15 +15,7 @@ var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리
 
 // 지도에 확대 축소 컨트롤 생성
 let zoomControl = new kakao.maps.ZoomControl();
-
-// 지도의 우측에 확대 축소 컨트롤 추가
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-
-
-
-
-
-
 
 
 /*
@@ -37,188 +29,211 @@ const dataSet = [
 		address: "충남 논산시 은진면 대학로 120",
 		url: "http://naver.me/xlWIs5zI",
 		image_url: "https://youtu.be/ZeEXwgcZ_Vk",
-		category: "중식",
+		category: '중식',
 	},
 	{
 		title: "오구오오",
 		address: "충남 논산시 은진면 대학로 138",
 		url: "http://naver.me/53XgXLjX",
 		image_url: "https://youtu.be/OoOegRMaBME",
-		category: "한식",
+		category: '한식',
 	},
 	{
 		title: "한솥도시락 건양대앞점",
 		address: "충남 논산시 은진면 와야길 9",
 		url: "http://naver.me/FrKAVg62",
 		image_url: "https://youtu.be/dtYexNnsGvI",
-		category: "양식",
+		category: '양식',
 	},
 	{
 		title: "동해안동태찜 논산본점",
 		address: "충남 논산시 시민로84번길 3",
 		url: "http://naver.me/xivSVGIO",
 		image_url: "https://youtu.be/7Du6stvhQ5g",
-		category: "한식",
+		category: '한식',
 	},
 	{
 		title: "국이랑밥이랑",
 		address: "충남 논산시 은진면 대학로 136",
 		url: "http://naver.me/xbwaAHtq",
 		image_url: "https://youtu.be/asDQQWRS1uY",
-		category: "한식",
+		category: '한식',
 	},
 	{
 		title: "내동춘천닭갈비",
 		address: "충남 논산시 시민로132번길 52",
 		url: "http://naver.me/G14gIeta",
 		image_url: "https://youtu.be/Md6LQFtfApY",
-		category: "한식",
+		category: '한식',
 	},
 ];
 
-
-
-
-
-
-
-
 /*
-******************************************************************************
+**********************************************************
 3. 여러개 마커 찍기
-    주소 - 좌표 변환
+  * 주소 - 좌표 변환
 */
-// 주소 - 좌표 변환 객체를 생성합니다.
-async function setMap() {
-	for (var i = 0; i < dataSet.length; i++) {
-		let position = await getCoordsByAddress(dataSet[i].address);
-		console.log(position);
 
-	  	// 마커를 생성합니다
-		var marker = new kakao.maps.Marker({
-			map: map, // 마커를 표시할 지도
-			// position: positions[i].latlng, // 마커를 표시할 위치
-			position: position,
-			// title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-		});
-	}
-}
-
+// 주소-좌표 변환 객체를 생성합니다
 var geocoder = new kakao.maps.services.Geocoder();
+
+// 주소-좌표 변환 함수
 function getCoordsByAddress(address) {
 	return new Promise((resolve, reject) => {
-		geocoder.addressSearch(address, function (result, status) {
-			if (status === kakao.maps.services.Status.OK) {
+    	// 주소로 좌표를 검색합니다
+		geocoder.addressSearch(
+			address,
+			function (result, status) {
+    		// 정상적으로 검색이 완료됐으면
+    		if (status === kakao.maps.services.Status.OK) {
 				var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-				return resolve(coords);
-			}
-			reject(new Error("getCoordsByAddress Error: not valid Address"));
-		});
+				resolve(coords);
+      		}
+			reject(new Error("getCoordsByAddress Error: invaild Address"));
+    		}
+		);
 	});
 }
 
-setMap();
-
-
-
-
-
-
 
 /* 
-******************************************************************************
+*************************************************************
 4. 마커에 인포윈도우 붙이기
 */
 
-async function setMap() {
+function getContent(data) {
+  let replaceUrl = data.image_url;
+  let finUrl = "";
+  replaceUrl = replaceUrl.replace("https://youtu.be/", "");
+  replaceUrl = replaceUrl.replace("https://www.youtube.com/embed/", "");
+  replaceUrl = replaceUrl.replace("https://www.youtube.com/watch?v=", "");
+  finUrl = replaceUrl.split("&")[0];
+
+  // 인포윈도우 가공하기
+  return `
+  <div class="infowindow">
+      <div class="infowindow-img-container">
+        <img
+          src="https://img.youtube.com/vi/${finUrl}/mqdefault.jpg"
+          class="infowindow-img"
+        />
+      </div>
+      <div class="infowindow-body">
+        <h5 class="infowindow-title">${data.title}</h5>
+        <p class="infowindow-address">${data.address}</p>
+        <a href="${data.url}" class="infowindow-btn" target="_blank">가게정보</a>
+      </div>
+    </div>
+  `;
+}
+
+async function setMap(dataSet) {
+  //markerArray = [];
+  // infowindowArray = [];
+
 	for (var i = 0; i < dataSet.length; i++) {
-		let position = await getCoordsByAddress(dataSet[i].address);
+    // 마커를 생성합니다
+    	const coords = await getCoordsByAddress(dataSet[i].address);
+    	var marker = new kakao.maps.Marker({
+      		map: map, // 마커를 표시할 지도
+      		position: coords, // 마커를 표시할 위치
+    	});
 
-	  // 마커를 생성합니다
-		var marker = new kakao.maps.Marker({
-			map: map, // 마커를 표시할 지도
-			position: position, // 마커를 표시할 위치
-		});
+    	markerArray.push(marker);
 
-	  // 마커에 표시할 인포윈도우를 생성합니다
-		var infowindow = new kakao.maps.InfoWindow({
-			content: getContent(dataSet[i]), // 인포윈도우에 표시할 내용
-			disableAutoPan: true, // 인포윈도우를 열 때 지도가 자동으로 패닝하지 않을지의 여부 (기본값: false)
-		});
+    // 마커에 표시할 인포윈도우를 생성합니다
+    	var infowindow = new kakao.maps.InfoWindow({
+      		content: getContent(dataSet[i]), // 인포윈도우에 표시할 내용
+    	});
 
-		infowindowArray.push(infowindow);
+    	infowindowArray.push(infowindow);
 
-	  // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
-	  // 이벤트 리스너로는 클로저를 만들어 등록합니다
-	  // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
-		kakao.maps.event.addListener(
-			marker,
+    // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
+    // 이벤트 리스너로는 클로저를 만들어 등록합니다
+    // for문에서 클로저를 만들어 주지 않으면 마지막 마커에만 이벤트가 등록됩니다
+    	kakao.maps.event.addListener(
+    		marker,
 			"click",
-			makeOverListener(map, marker, infowindow, position)
+    		makeOverListener(map, marker, infowindow, coords)
+    	);
+    	kakao.maps.event.addListener(
+			map,
+			"click",
+			makeOutListener(infowindow)
 		);
-	  	// 커스텀: 맵을 클릭하면 현재 나타난 인포윈도우가 없어지게끔
-		kakao.maps.event.addListener(map, "click", makeOutListener(infowindow));
 	}
 }
 
-  // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
-	/* 
-	커스텀
-	1. 클릭시 다른 인포윈도우 닫기
-	2. 클릭한 곳으로 지도 중심 이동하기
-	*/
-
-function makeOverListener(map, marker, infowindow, position) {
+// 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+// 1. 클릭시 다른 인포윈도우 닫기
+// 2. 클릭한 곳으로 지도 중심 옮기기
+function makeOverListener(map, marker, infowindow, coords) {
 	return function () {
-		// 1. 클릭시 다른 인포윈도우 닫기
-		closeInfowindow();
-		infowindow.open(map, marker);
-	  	// 2. 클릭한 곳으로 짇 중심 이동하기
-		map.panTo(position);
+    	closeInfoWindow();
+    	infowindow.open(map, marker);
+    	map.panTo(coords);
 	};
 }
 
-  	// 커스텀
-  	// 1. 클릭시 다른 인포윈도우 닫기
 let infowindowArray = [];
-function closeInfowindow() {
-	for (let infowindow of infowindowArray) {
-		infowindow.close();
+function closeInfoWindow() {
+	for (let info of infowindowArray) {
+    	info.close();
 	}
 }
 
-  // 인포윈도우를 닫는 클로저를 만드는 함수입니다
+// 인포윈도우를 닫는 클로저를 만드는 함수입니다
 function makeOutListener(infowindow) {
 	return function () {
-		infowindow.close();
+    	infowindow.close();
 	};
 }
 
-  // HTML 코드로 바꾸는 함수
-function getContent(data) {
-	let videoId = "";
-	let replaceUrl = data.image_url;
-	replaceUrl = replaceUrl.replace("https://youtu.be/", "");
-	replaceUrl = replaceUrl.replace("https://www.youtube.com/embed/", "");
-	replaceUrl = replaceUrl.replace("https://www.youtube.com/watch?v=", "");
-	videoId = replaceUrl.split("&")[0];
+/*
+**********************************************
+5. 카테고리 분류
+*/
 
-	const result = 
-	`<div class="infowindow">
-		<div class="infowindow-img-container">
-			<img src="https://img.youtube.com/vi/${videoId}/sddefault.jpg" class="infowindow-img" alt="...">
-		</div>
-		<div class="infowindow-body">
-			<h2 class="infowindow-title">${data.title}</h2>
-			<p class="infowindow-text">${data.address}</p>
-			<a href="${data.url}" target="_blank" class="infowindow-btn">가게정보</a>
-		</div>
-	</div>`;
-	return result;
+// 카테고리
+const categoryMap = {
+	korea: '한식',
+	china: '중식',
+	japan: '일식',
+	america: '양식',
+	wheat: '분식',
+	meat: '구이',
+	sushi: '회/초밥',
+	etc: '기타',
+};
+
+const categoryList = document.querySelector(".category-list");
+categoryList.addEventListener("click", categoryHandler);
+
+async function categoryHandler(event) {
+	const categoryId = categoryMap[event.target.id];
+
+  // 데이터 분류
+	let categorizedDataSet = [];
+	for (let data of dataSet) {
+    	if (data.category === categoryId) {
+			categorizedDataSet.push(data);
+    	}
+	}
+
+  // 기존 마커 삭제
+	closeMarker();
+
+  // 기존 인포윈도우 닫기
+	closeInfoWindow();
+
+	setMap(categorizedDataSet);
 }
 
-setMap();
+let markerArray = [];
+function closeMarker() {
+	for (let marker of markerArray) {
+    	marker.setMap(null);
+	}
+}
 
-
-
-
+setMap(dataSet);
