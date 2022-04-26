066e9e54c931bc235029cf9d5f0fb1b56c88c470
Wonseok Park <jp0301@naver.com>
2022-04-26 10:24:28 +0900

    Delete googlesearch.js

--- googlesearch.js
+++
@@ -1,9 +0,0 @@
-let query = document.querySelector('.query');
-let searchText = document.querySelector('.searchText');
-
-function enterkey() {
-    if(window.event.keyCode == 13) {
-        let url = 'http://www.google.com/search?q=' + query.value;
-        this.window.open(url, '_self');
-    }
-}
